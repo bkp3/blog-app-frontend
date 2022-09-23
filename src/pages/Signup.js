7b3f2ad18c2b09b20ad16e713c6cdcc73c3904a4
bkp3 <bholakumar.7372@gmail.com>
2022-09-23 23:35:17 +0530

    Two Way data binding

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,11 +1,59 @@
+import { useEffect, useState } from "react";
 import { Button, Card, CardBody, CardHeader, Col, Container, Form, FormFeedback, FormGroup, Input, Label, Row } from "reactstrap";
 import Base from "../components/Base";
 
 const Signup = () => {
+
+    const [data, setData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        about: ''
+    })
+
+    const [error, setError] = useState({
+        errors: {},
+        isError: false
+    })
+
+
+
+    useEffect(() => {
+        console.log(data);
+    }, [data])
+
+    //handle change
+    const handleChange = (event, property) => {
+
+        setData({ ...data, [property]: event.target.value })
+    }
+
+    //resetting the form
+    const resetData = () => {
+        setData({
+            name: '',
+            email: '',
+            password: '',
+            about: ''
+        })
+    }
+    //submitting the form
+    const submitForm = (event) => {
+        event.preventDefault()
+
+        console.log(data);
+
+        //data validate
+
+        //call server api for sending the data
+
+    }
+
     return (
         <Base>
             <Container>
                 <Row className="mt-4">
+                    {JSON.stringify(data)}
                     <Col sm={{ size: 6, offset: 3 }}>
                         <Card outline color="dark">
                             <CardHeader>
@@ -13,36 +61,57 @@ const Signup = () => {
                             </CardHeader>
                             <CardBody>
                                 {/* creating a form */}
-                                <Form>
+                                <Form onSubmit={submitForm}>
 
                                     {/* Name field */}
                                     <FormGroup>
                                         <Label for="name">Enter Name</Label>
-                                        <Input type="text" placeholder="Enter here" id="name" />
+                                        <Input
+                                            type="text"
+                                            placeholder="Enter here"
+                                            id="name"
+                                            onChange={(e) => handleChange(e, 'name')}
+                                            value={data.name}
+                                        />
                                     </FormGroup>
 
                                     {/* email field */}
                                     <FormGroup>
                                         <Label for="email">Enter Email</Label>
-                                        <Input type="email" placeholder="Enter here" id="email" />
+                                        <Input
+                                            type="email"
+                                            placeholder="Enter here"
+                                            id="email"
+                                            onChange={(e) => handleChange(e, 'email')}
+                                            value={data.email} />
                                     </FormGroup>
 
                                     {/* password field */}
                                     <FormGroup>
                                         <Label for="password">Enter Password</Label>
-                                        <Input type="password" placeholder="Enter here" id="password" />
+                                        <Input
+                                            type="password"
+                                            placeholder="Enter here"
+                                            id="password"
+                                            onChange={(e) => handleChange(e, 'password')}
+                                            value={data.password} />
                                     </FormGroup>
 
                                     {/* about field */}
                                     <FormGroup>
                                         <Label for="about">Enter About</Label>
-                                        <Input type="textarea" placeholder="Enter here" id="about" />
+                                        <Input
+                                            type="textarea"
+                                            placeholder="Enter here"
+                                            id="about"
+                                            onChange={(e) => handleChange(e, 'about')}
+                                            value={data.about} />
                                     </FormGroup>
 
                                     {/* button submit */}
                                     <Container className="text-center">
                                         <Button color="dark">Register</Button>
-                                        <Button color="secondary" type="reset" className="ms-2">Reset</Button>
+                                        <Button onClick={resetData} color="secondary" type="reset" className="ms-2">Reset</Button>
                                     </Container>
                                 </Form>
                             </CardBody>
