60a675e98232b0d3d0b3121161f28a76ca53ee9d
bkp3 <bholakumar.7372@gmail.com>
2022-10-05 16:48:54 +0530

    Pagination

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -6,8 +6,8 @@ function Post({ post = { title: "This is desfault post title", content: "This is
         <Card className='border-0 shadow-sm mt-3'>
             <CardBody>
                 <h1>{post.title}</h1>
-                <CardText>
-                    {post.content.substring(0,200)}...
+                <CardText dangerouslySetInnerHTML={{ __html: post.content.substring(0,60)+"..." } }>
+
                 </CardText>
                 <div>
                     <Button>Read More</Button>
